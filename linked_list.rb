ac976101e18e126e9b28dfd5051feb050367bae7
clair13 <theclairvoyant1313@yahoo.gr>
2019-07-13 18:57:35 +0300

    Implement at, pop, contains methods

--- linked_list.rb
+++ linked_list.rb
@@ -51,4 +51,36 @@ class LinkedList
   def tail
     @tail.value
   end
+
+  def at(index)
+    i = 0
+    current_node = @head
+    until current_node.nil?
+      if i == index
+        return current_node
+      else
+        i += 1
+        current_node = current_node.next_node
+      end
+    end
+  end
+
+  def pop
+    current_node = @head
+    until current_node.next_node == @tail
+      current_node = current_node.next_node
+    end
+    @tail = current_node
+    @tail.next_node = nil
+  end
+
+  def contains?(value)
+    current_node = @head
+      until current_node == nil
+        return true if current_node.value ==value
+        current_node = current_node.next_node
+      end
+    false
+  end
+
 end
